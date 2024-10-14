abab3178e9402c79d8aab01f5ebb1571f4a641ff
Nahum <toodoo1703@gmail.com>
2024-10-14 01:01:19 -0600

    Prototype

---
+++ JS/redirectionsManager.js
@@ -0,0 +1,16 @@
+function QueEs()
+{
+    window.location.href = "../HTML/queEs.html"
+}
+function Tipos()
+{
+    window.location.href = "../HTML/tipos.html"
+}
+function ComoAplicar()
+{
+    window.location.href = "../HTML/comoAplicar.html"
+}
+function PorDonde()
+{
+    window.location.href = "../HTML/porDonde.html"
+}
