d0411e7e030c965e776327feff974e15caf37d1d
Nahum <toodoo1703@gmail.com>
2024-10-18 18:37:35 -0600

    Analytics :)

--- responsabilidad/app/page.tsx
+++ responsabilidad/app/page.tsx
@@ -1,5 +1,23 @@
 import Image from "next/image";
+import { Analytics } from '@vercel/analytics/react';
 
+export function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
